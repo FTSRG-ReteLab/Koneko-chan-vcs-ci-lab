f4789b85ffc654918ec9263d1336a7329ce7d00e
zongolista <zongolista@gmail.com>
2017-03-06 08:50:41 -0800

    tachograph added

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -1,5 +1,9 @@
 package hu.bme.mit.train.controller;
 
+import java.util.Date;
+
+import com.google.common.collect.Table;
+
 import hu.bme.mit.train.interfaces.TrainController;
 
 public class TrainControllerImpl implements TrainController {
@@ -7,6 +11,14 @@ public class TrainControllerImpl implements TrainController {
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
+	private Table<Integer, Integer, Integer> tachograph;
+	private int id = 0; 
+	
+	public void tableSave() {
+		tachograph.put(id, 0, (int) (new Date().getTime()/1000));
+		tachograph.put(id, 1, step);
+		tachograph.put(id++, 2, referenceSpeed);		
+	}
 
 	@Override
 	public void followSpeed() {
